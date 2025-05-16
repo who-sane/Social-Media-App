f7ebbae3fa6b2570a8cbb0c0ae7d2e6c71c729e8
who-sane <103615588@student.swin.edu.au>
2025-05-16 13:06:57 +1000

    added _redirects and improved home mobile accessibility for title text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ export const Home = () => {
     // main containter for home page 
     <div className="pt-1">
       <h3
-        className="text-3xl font-bold mb-6 text-center h-10 bg-linear-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent"
+        className="text-3xl font-bold mb-6 text-center bg-linear-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent"
         style={{ fontFamily: '"Pixelify Sans", monospace' }}
       >
         A Community <span>By Developers</span>, <span>For Developers</span>
