066bc58da1412029a299f7b559dfea699e28aad8
who-sane <103615588@student.swin.edu.au>
2025-05-16 13:46:44 +1000

    enforcing protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { PostPage } from "./pages/PostPage";
 import { CreateCommunityPage } from "./pages/CreateCommunityPage";
 import { CommunitiesPage } from "./pages/CommunitiesPage";
 import { CommunityPage } from "./pages/CommunityPage";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 function App() {
   return (
     <div className="min-h-screen bg-rose-50 text-black-900 transition-opacity duration-700 pt-20">
@@ -13,9 +14,17 @@ function App() {
       <div className="container mx-auto px-4 py-6">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/create" element={<CreatePostPage />} />
+          <Route path="/create" element={
+            <ProtectedRoute>
+              <CreatePostPage />
+            </ProtectedRoute>
+          } />
           <Route path="/post/:id" element={<PostPage />} />
-          <Route path="/community/create" element={<CreateCommunityPage />} />
+          <Route path="/community/create" element={
+            <ProtectedRoute>
+              <CreateCommunityPage />
+            </ProtectedRoute>
+          } />
           <Route path="/communities" element={<CommunitiesPage />} />
           <Route path="/community/:id" element={<CommunityPage />} />
         </Routes>
